10bf2514770b514b7ee1f28b0041b42f6ac91669
SnickeyX <s2152674@ed.ac.uk>
2023-01-14 17:19:43 +0000

    cd

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,24 @@ fn main() {
         let mut command_with_args = input.trim().split_whitespace();
         let command = command_with_args.next().unwrap();
         let args = command_with_args;
-        // Running the inputted command in a new process (will not work on Windows without going through powershell or cmd)
-        if let Ok(mut curr_process) = std::process::Command::new(command).args(args).spawn() {
-            // Waiting for the process to finish
-            curr_process.wait().expect("Failed to wait on child");
-        } else {
-            println!("Command not found");
+        // To handle shell builtins - this allows us change the state of the shell rather than the specific process handling the command
+        match command {
+            "cd" => {
+                let path = args.peekable().peek().map_or("/", |x| *x);
+                if let Err(e) = std::env::set_current_dir(path) {
+                    println!("Failed: {}", e);
+                }
+            }
+            command => {
+                // Running the inputted command in a new process (will not work on Windows without going through powershell or cmd)
+                if let Ok(mut curr_process) = std::process::Command::new(command).args(args).spawn()
+                {
+                    // Waiting for the process to finish
+                    curr_process.wait().expect("Failed to wait on child");
+                } else {
+                    println!("Command not found");
+                }
+            }
         }
     }
 }
