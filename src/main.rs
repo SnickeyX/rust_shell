0367534de7c82885fc2425e2db6a3e10b65da0b7
SnickeyX <s2152674@ed.ac.uk>
2023-01-14 16:54:27 +0000

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let command = input.trim();
-    // Running the inputted command in a new process (will not work on Windows without going through powershell or cmd)
-    std::process::Command::new(command).spawn().unwrap();
+    loop {
+        print!(">");
+        // Flushing the stdout buffer to make sure the prompt is printed
+        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let command = input.trim();
+        // Running the inputted command in a new process (will not work on Windows without going through powershell or cmd)
+        if let Ok(mut curr_process) = std::process::Command::new(command).spawn() {
+            // Waiting for the process to finish
+            curr_process.wait().expect("Failed to wait on child");
+        } else {
+            println!("Command not found");
+        }
+    }
 }
