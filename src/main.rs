8419b5157e010cae31e57138d1140136a8021302
SnickeyX <s2152674@ed.ac.uk>
2023-01-14 17:00:23 +0000

    var args

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ fn main() {
         std::io::Write::flush(&mut std::io::stdout()).unwrap();
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-        let command = input.trim();
+        let mut command_with_args = input.trim().split_whitespace();
+        let command = command_with_args.next().unwrap();
+        let args = command_with_args;
         // Running the inputted command in a new process (will not work on Windows without going through powershell or cmd)
-        if let Ok(mut curr_process) = std::process::Command::new(command).spawn() {
+        if let Ok(mut curr_process) = std::process::Command::new(command).args(args).spawn() {
             // Waiting for the process to finish
             curr_process.wait().expect("Failed to wait on child");
         } else {
